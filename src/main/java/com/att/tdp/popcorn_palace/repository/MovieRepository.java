a93460ed1d0aacb4781b643b44d19ef57b2dc271
Gabrielab5 <87898816+Gabrielab5@users.noreply.github.com>
2025-03-18 22:05:44 +0200

    updated fixes

--- src/main/java/com/att/tdp/popcorn_palace/repository/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.att.tdp.popcorn_palace.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository<Movie> extends JpaRepository<Movie, Long> {}
-
